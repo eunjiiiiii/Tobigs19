3f1b61cffb5aa3c950c0d46913d55af84a940822
eunjiiiiii <ej27470235@gmail.com>
2023-02-28 16:08:41 +0900

    [백준] 1463. 1로 만들기

---
+++ Algorithm/Week5/[백준] 1463. 1로 만들기/[백준] 1463. 1로 만들기.py
@@ -0,0 +1,16 @@
+def calc(x):
+    if x in cnt.keys():
+        return cnt[x]
+    if (x % 3 == 0) and (x % 2 == 0):
+        return min(calc(x//3) + 1, calc(n//2) + 1)
+    elif x % 3 == 0:
+        return min(calc(x//3)+1, calc(x-1)+1)
+    elif x % 2 == 0:
+        return min(calc(x//2)+1, calc(n-1)+1)
+    else:
+        cnt[x] = calc(x-1) + 1
+    return cnt[x]
+
+n = int(input())
+cnt = {1: 0}
+print(calc(n))
